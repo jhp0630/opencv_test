fc7d722b5edc6b05b79158bf8ad5b9ca6db83403
jhp0630 <marine04100@gmail.com>
2021-01-27 16:30:07 +0900

    mnist 손글씨 인식 추가

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ Visual Studio 2010 버전 이상부터는 MS의 Concurrency라는 이름의 멀티 쓰레드 기법�
 
 using namespace cv;
 using namespace std;
+using namespace cv::ml;
+using namespace cv::dnn;
 
 
 //int main(int, char**)
@@ -100,9 +102,9 @@ Mat and image load
 pixel access
 */
 
-int main(int, char)
-{
-	Mat img = imread("ss.jpg");
+//int main(int, char)
+//{
+//	Mat img = imread("ss.jpg");
 	
 
 	///* using data */
@@ -181,13 +183,82 @@ int main(int, char)
 	//	(*itd)[2] = 255 - bgr[2];
 
 	//}
-
 	
 
+//
+//	imshow("img", img);
+//	waitKey(0);
+//	
+//	return 0;
+//  
+//}
+
+
+
+
+
+/*
+DNN
+*/
+
+void on_mouse(int event, int x, int y, int flags, void* userdata);
+
+int main()
+{
+	Net net = readNet("mnist_cnn.pb");
+
+	if (net.empty()) {
+		cerr << "Network load failed!" << endl;
+		return -1;
+	}
+
+	Mat img = Mat::zeros(400, 400, CV_8UC1);
 
 	imshow("img", img);
-	waitKey(0);
-	
+	setMouseCallback("img", on_mouse, (void*)&img);
+
+	while (true) {
+		int c = waitKey();
+
+		if (c == 27) {
+			break;
+		}
+		else if (c == ' ') {
+			Mat blob = blobFromImage(img, 1 / 255.f, Size(28, 28));
+			net.setInput(blob);
+			Mat prob = net.forward();
+
+			double maxVal;
+			Point maxLoc;
+			minMaxLoc(prob, NULL, &maxVal, NULL, &maxLoc);
+			int digit = maxLoc.x;
+
+			cout << digit << " (" << maxVal * 100 << "%)" << endl;
+
+			img.setTo(0);
+			imshow("img", img);
+		}
+	}
+
 	return 0;
-  
+}
+
+Point ptPrev(-1, -1);
+
+void on_mouse(int event, int x, int y, int flags, void* userdata)
+{
+	Mat img = *(Mat*)userdata;
+
+	if (event == EVENT_LBUTTONDOWN) {
+		ptPrev = Point(x, y);
+	}
+	else if (event == EVENT_LBUTTONUP) {
+		ptPrev = Point(-1, -1);
+	}
+	else if (event == EVENT_MOUSEMOVE && (flags & EVENT_FLAG_LBUTTON)) {
+		line(img, ptPrev, Point(x, y), Scalar::all(255), 40, LINE_AA, 0);
+		ptPrev = Point(x, y);
+
+		imshow("img", img);
+	}
 }
